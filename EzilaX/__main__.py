90acd57525a1b63a9a0aef61e4d0f057f9567600
Setupbeta <94524084+Setupbeta@users.noreply.github.com>
2021-11-19 02:52:35 -0800

    Update __main__.py

--- EzilaX/__main__.py
+++ EzilaX/__main__.py
@@ -159,7 +159,11 @@ def send_help(chat_id, text, keyboard=None):
     if not keyboard:
         keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
     dispatcher.bot.send_message(
-        chat_id=chat_id, text=text, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard
+        chat_id=chat_id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+        reply_markup=keyboard,
     )
 
 
